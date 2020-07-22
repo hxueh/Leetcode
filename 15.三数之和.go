3319e18d1bff7b94ed7c80891ff36b9d13eddb2c
Nathan Huang <nathan.huang96@gmail.com>
2020-07-23 00:13:04 +0800

    add three sum

---
+++ 15.三数之和.go
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=golang
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+
+import "sort"
+
+type Three struct {
+	first, second, third int
+}
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := make([][]int, 0)
+	resMap := make(map[Three]struct{})
+	for idx := 0; idx < len(nums) - 2; idx++ {
+		left, right := idx + 1, len(nums) - 1
+		for left < right {
+			sum := nums[idx] + nums[left] + nums[right]
+			if sum == 0 {
+				curThree := Three{
+					first: nums[idx],
+					second: nums[left],
+					third: nums[right],
+				}
+				resMap[curThree] = struct{}{}
+                left++
+			} else if sum > 0 {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+	for key, _ := range resMap {
+		res = append(res, []int{key.first, key.second, key.third})
+	}
+	return res
+}
+
+// @lc code=end
