b0a451852a3d075515c0dddc1b28922f01348793
Nathan Huang <nathan.huang96@gmail.com>
2020-07-23 18:04:31 +0800

    add remove duplicates from sorted array

---
+++ 26.删除排序数组中的重复项.go
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] 删除排序数组中的重复项
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[i] != nums[j] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+
+// Time complexity O(N)
+// Space complexity O(1)
+
+// @lc code=end
