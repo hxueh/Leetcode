56510598484cf21d7ed952797bb93d39dd81639b
Nathan Huang <nathan.huang96@gmail.com>
2020-07-23 14:05:09 +0800

    remove nth node from end of list

---
+++ 19.删除链表的倒数第n个节点.go
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=golang
+ *
+ * [19] 删除链表的倒数第N个节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	fast, slow := head, head
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	if fast == nil {
+		return head.Next
+	}
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return head
+}
+
+// Time complexity O(N)
+// Space complexity O(1)
+
+// @lc code=end
