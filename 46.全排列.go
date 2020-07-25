446cfce6fcd42b3f3839f82f44471d78e9d90c89
Nathan Huang <nathan.huang96@gmail.com>
2020-07-26 01:27:05 +0800

    add 46

---
+++ 46.全排列.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=golang
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+func copyAndPop(nums []int, popIdx int) []int {
+	res := make([]int, len(nums) - 1)
+	copy(res, nums[:popIdx])
+	copy(res[popIdx:], nums[popIdx + 1:])
+	return res
+}
+
+func helper(nums []int, curRes []int, res *[][]int) {
+	if len(nums) == 0 {
+		*res = append(*res, curRes)
+	} else {
+		for idx, num := range nums {
+			newNums := copyAndPop(nums, idx)
+			newCurRes := append(curRes, num)
+			helper(newNums, newCurRes, res)
+		}
+	}
+}
+
+func permute(nums []int) [][]int {
+	res := make([][]int, 0)
+    curRes := make([]int, 0)
+	helper(nums, curRes, &res)
+	return res
+}
+// @lc code=end
