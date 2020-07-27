5330cb921850c2c31a8862dbaa41bdbe8ca85d2a
Nathan Huang <nathan.huang96@gmail.com>
2020-07-27 18:11:33 +0800

    add my pow

---
+++ 50.pow-x-n.go
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=golang
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+func myPow(x float64, n int) float64 {
+	if n < 0 {
+		return 1.0 / myPow(x, -n)
+	}
+	if n == 0 {
+		return 1
+	}
+	if n == 1 {
+		return x
+	}
+	half := myPow(x, n / 2)
+	return half * half * myPow(x, n % 2)
+}
+// @lc code=end
