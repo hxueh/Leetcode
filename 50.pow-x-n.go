5bb536b723b0f1360db0f8fcba1cb5a8646dc518
Nathan Huang <nathan.huang96@gmail.com>
2020-07-30 14:11:55 +0800

    update 50

--- 50.pow-x-n.go
+++ 50.pow-x-n.go
@@ -9,17 +9,18 @@ func myPow(x float64, n int) float64 {
 	if n < 0 {
 		return 1.0 / myPow(x, -n)
 	}
-	if n == 0 {
-		return 1
-	}
-	if n == 1 {
-		return x
-	}
-	half := myPow(x, n / 2)
-	return half * half * myPow(x, n % 2)
+    res := 1.0
+    for n > 0 {
+        if n % 2 > 0 {
+            res = x * res
+        }
+        x *= x
+        n /= 2
+    }
+	return res
 }
 
 // Time complexity O(logN)
-// Space complexity O(logN)
+// Space complexity O(1)
 
 // @lc code=end
