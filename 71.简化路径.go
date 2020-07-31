3df7ebcbd1fba0cbb01cb8731ae79d55abbd342f
Nathan Huang <nathan.huang96@gmail.com>
2020-07-31 10:56:25 +0800

    add 71

---
+++ 71.简化路径.go
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=71 lang=golang
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+import (
+	"strings"
+)
+
+func simplifyPath(path string) string {
+	path = path + "/"
+	paths := make([]string, 0)
+	start := 0
+	for end := 0; end < len(path); end++ {
+		if path[end] == '/' {
+			if start+1 < end {
+				paths = append(paths, string(path[start+1:end]))
+			}
+			start = end
+		}
+	}
+	idx := 0
+	for idx < len(paths) {
+		switch paths[idx] {
+		case ".":
+			paths = append(paths[:idx], paths[idx+1:]...)
+		case "..":
+			if idx < 1 {
+				paths = paths[idx+1:]
+			} else {
+				paths = append(paths[:idx-1], paths[idx+1:]...)
+				idx--
+			}
+		default:
+			idx++
+		}
+	}
+	return "/" + strings.Join(paths, "/")
+}
+
+// @lc code=end
